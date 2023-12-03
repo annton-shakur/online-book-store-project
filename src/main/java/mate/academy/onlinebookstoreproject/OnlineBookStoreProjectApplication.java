b9fe7d95b4fdf583ac202161b9ccab0dac100c5e
Anton Dudar <dudar.anton4uk@gmail.com>
2023-12-03 23:13:15 +0200

    basic version

---
+++ src/main/java/mate/academy/onlinebookstoreproject/OnlineBookStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.onlinebookstoreproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/onlinebookstoreproject/OnlineBookStoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.onlinebookstoreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
