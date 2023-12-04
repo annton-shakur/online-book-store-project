d4f99d9f6b8da0c621cce2f8ad75bbd56571faf5
Anton Dudar <dudar.anton4uk@gmail.com>
2023-12-04 11:11:01 +0200

    added impl folders and @Table definition (fixed * imports)

--- src/main/java/mate/academy/onlinebookstoreproject/model/Book.java
+++ src/main/java/mate/academy/onlinebookstoreproject/model/Book.java
@@ -1,7 +1,11 @@
 package mate.academy.onlinebookstoreproject.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
 import org.springframework.lang.NonNull;
