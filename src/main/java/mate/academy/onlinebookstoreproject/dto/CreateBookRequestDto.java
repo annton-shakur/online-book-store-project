0d06ecd48cb7afa3a2e663cdf81006b10c72c3cd
Anton Dudar <dudar.anton4uk@gmail.com>
2023-12-13 22:43:26 +0200

    added globalExceptionHandler and data validation

--- src/main/java/mate/academy/onlinebookstoreproject/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/onlinebookstoreproject/dto/CreateBookRequestDto.java
@@ -1,13 +1,21 @@
 package mate.academy.onlinebookstoreproject.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    private static final String CANNOT_BE_NULL_MSG = "cannot be null";
+    @NotNull (message = CANNOT_BE_NULL_MSG)
     private String title;
+    @NotNull (message = CANNOT_BE_NULL_MSG)
     private String author;
+    @NotNull (message = CANNOT_BE_NULL_MSG)
     private String isbn;
+    @NotNull (message = CANNOT_BE_NULL_MSG)
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
