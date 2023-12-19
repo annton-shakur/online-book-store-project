61152a81742c5868a02300a70dc2c68748d73d86
Anton Dudar <dudar.anton4uk@gmail.com>
2023-12-19 14:35:12 +0200

    added email check to the userservice

--- src/main/java/mate/academy/onlinebookstoreproject/repository/UserRepository.java
+++ src/main/java/mate/academy/onlinebookstoreproject/repository/UserRepository.java
@@ -4,4 +4,5 @@ import mate.academy.onlinebookstoreproject.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findUserByEmail(String email);
 }
--- src/main/java/mate/academy/onlinebookstoreproject/service/impl/UserServiceImpl.java
+++ src/main/java/mate/academy/onlinebookstoreproject/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package mate.academy.onlinebookstoreproject.service.impl;
 import lombok.RequiredArgsConstructor;
 import mate.academy.onlinebookstoreproject.dto.user.UserRegistrationRequestDto;
 import mate.academy.onlinebookstoreproject.dto.user.UserResponseDto;
+import mate.academy.onlinebookstoreproject.exception.RegistrationException;
 import mate.academy.onlinebookstoreproject.mapper.UserMapper;
 import mate.academy.onlinebookstoreproject.model.User;
 import mate.academy.onlinebookstoreproject.repository.UserRepository;
@@ -12,11 +13,16 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
+    public static final String EMAIL_EXIST_MSG
+            = "The user with the provided email already exists!";
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
     @Override
     public UserResponseDto save(UserRegistrationRequestDto requestDto) {
+        if (userRepository.findUserByEmail(requestDto.getEmail()) != null) {
+            throw new RegistrationException(EMAIL_EXIST_MSG);
+        }
         User user = userMapper.toModel(requestDto);
         userRepository.save(user);
         return userMapper.toDto(user);
