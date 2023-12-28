e54f18df9a72953ab224e4f9f5c2df4908b5a031
Anton Dudar <dudar.anton4uk@gmail.com>
2023-12-28 14:23:01 +0200

    style: renamed some constants error messages in dtos

--- src/main/java/mate/academy/onlinebookstoreproject/dto/book/CreateBookRequestDto.java
+++ src/main/java/mate/academy/onlinebookstoreproject/dto/book/CreateBookRequestDto.java
@@ -1,23 +1,23 @@
 package mate.academy.onlinebookstoreproject.dto.book;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
-    private static final String CANNOT_BE_NULL_MSG = "cannot be null";
-    @NotNull (message = CANNOT_BE_NULL_MSG)
+    private static final String CANNOT_BE_NULL_MSG = "cannot be null or blank";
+    @NotBlank(message = CANNOT_BE_NULL_MSG)
     private String title;
-    @NotNull (message = CANNOT_BE_NULL_MSG)
+    @NotBlank (message = CANNOT_BE_NULL_MSG)
     private String author;
-    @NotNull (message = CANNOT_BE_NULL_MSG)
+    @NotBlank (message = CANNOT_BE_NULL_MSG)
     @ISBN
     private String isbn;
-    @NotNull (message = CANNOT_BE_NULL_MSG)
+    @NotBlank (message = CANNOT_BE_NULL_MSG)
     @Min(0)
     private BigDecimal price;
     @NotEmpty
