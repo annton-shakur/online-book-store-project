0c36d4947f77675c297f77f142cde41a088468f5
Anton Dudar <dudar.anton4uk@gmail.com>
2024-01-03 20:04:41 +0200

    feat: added Order & OrderItem entities with table creation yaml scripts

---
+++ src/main/java/mate/academy/onlinebookstoreproject/model/Order.java
@@ -0,0 +1,57 @@
+package mate.academy.onlinebookstoreproject.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.hibernate.type.SqlTypes;
+
+
+@Entity
+@Data
+@SQLDelete(sql = "UPDATE orders SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private Order.Status status;
+    @Column(nullable = false)
+    private BigDecimal total;
+    @CreationTimestamp
+    private LocalDateTime orderDate;
+    @Column(nullable = false)
+    private String shippingAddress;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(mappedBy = "order")
+    private Set<OrderItem> orderItems;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+    enum Status {
+        PENDING, COMPLETED, DELIVERED
+    }
+}
---
+++ src/main/java/mate/academy/onlinebookstoreproject/model/OrderItem.java
@@ -0,0 +1,44 @@
+package mate.academy.onlinebookstoreproject.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@SQLDelete(sql = "UPDATE order_items SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+    @Column(nullable = false)
+    private int quantity;
+    @Column(nullable = false)
+    private BigDecimal price;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+}
