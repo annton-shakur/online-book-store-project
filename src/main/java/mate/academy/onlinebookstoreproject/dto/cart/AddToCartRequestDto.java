27a5ebf4b9c3de7c2ed37f7a935eef3e3c91a527
Anton Dudar <dudar.anton4uk@gmail.com>
2024-01-03 10:58:22 +0200

    fix: changed @Min annotation for @Positive (for cart item dtos)

--- src/main/java/mate/academy/onlinebookstoreproject/dto/cart/AddToCartRequestDto.java
+++ src/main/java/mate/academy/onlinebookstoreproject/dto/cart/AddToCartRequestDto.java
@@ -1,11 +1,11 @@
 package mate.academy.onlinebookstoreproject.dto.cart;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class AddToCartRequestDto {
     private Long bookId;
-    @Min(value = 1, message = "cannot be less than 1")
+    @Positive(message = "must be positive")
     private int quantity;
 }
--- src/main/java/mate/academy/onlinebookstoreproject/dto/cart/CartItemUpdateDto.java
+++ src/main/java/mate/academy/onlinebookstoreproject/dto/cart/CartItemUpdateDto.java
@@ -1,10 +1,10 @@
 package mate.academy.onlinebookstoreproject.dto.cart;
 
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 
 @Data
 public class CartItemUpdateDto {
-    @Min(0)
+    @Positive(message = "must be positive")
     private int quantity;
 }
